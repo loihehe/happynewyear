15563b4f36586e34b97924443778b4dd11540a1c
loihehe <112553641+loihehe@users.noreply.github.com>
2024-02-09 23:48:34 +0700

    Add files via upload
    creat card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Hoài mít ướt';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happy New Year';
+const titleCard = 'Happy New Year 2024';
+const contentCard = 'Chúc mừng năm mới Hoài mít ướt, năm mới bớt báo kkk';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
